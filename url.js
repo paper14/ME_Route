ca8e1479065a3bbf21cec4b8bef24bb5f8574407
nicko <nicko.bsu@gmail.com>
2015-06-04 17:15:01 +0800

    updates

--- url.js
+++ url.js
@@ -1,11 +1,12 @@
-'use strict'
-
 var router = require('koa-router')();
 module.exports = function() {
 
   router
     .get('/', function * (next) {
-      yield this.render('layout');
+      yield this.render('pages/home');
+    })
+    .get('/about', function * (next) {
+      yield this.render('pages/about');
     });
 
   return router;
